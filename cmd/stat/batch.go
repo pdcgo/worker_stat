437b02f2873b0a409632821277a332e48b9ddbd0
vaziria <manorder123@gmail.com>
2026-02-11 12:39:34 +0700

    batch calculate

--- cmd/stat/batch.go
+++ cmd/stat/batch.go
@@ -48,6 +48,22 @@ func NewBatch(
 			map[string]batch_compute.Table{},
 		)
 
+		shopCreatedLog := batch_compute.NewTableSelect(
+			"shop_created_log",
+			`
+			select 
+				shop_id,
+				date(at) as day,
+				count(order_id) as tx_count,
+				sum(revenue_amount) as revenue_amount
+			from {{.orderCreatedLogTable}}
+			group by (shop_id, day)
+			`,
+			map[string]batch_compute.Table{
+				"orderCreatedLog": orderCreatedLog,
+			},
+		)
+
 		teamCreatedLog := batch_compute.NewTableSelect(
 			"team_created_log",
 			`
@@ -111,6 +127,22 @@ func NewBatch(
 			},
 		)
 
+		shopCompletedLog := batch_compute.NewTableSelect(
+			"shop_completed_log",
+			`
+			select 
+				shop_id,
+				date(at) as day,
+				count(order_id) as tx_count,
+				sum(revenue_amount) as revenue_amount
+			from {{.orderCompletedLogTable}}
+			group by (shop_id, day)
+			`,
+			map[string]batch_compute.Table{
+				"orderCompletedLog": orderCompletedLog,
+			},
+		)
+
 		teamOrderHoldLog := batch_compute.NewTableSelect(
 			"team_order_holds_log",
 			`
@@ -132,6 +164,27 @@ func NewBatch(
 			},
 		)
 
+		shopOrderHoldLog := batch_compute.NewTableSelect(
+			"shop_order_holds_log",
+			`
+			select 
+				COALESCE(cr.shop_id, com.shop_id) shop_id,
+				COALESCE(cr.day, com.day) as day,
+				COALESCE(com.tx_count, 0) as completed_count,
+				COALESCE(com.revenue_amount, 0) as completed_revenue_amount,
+				COALESCE(cr.tx_count, 0) as created_count,
+				COALESCE(cr.revenue_amount, 0) as created_revenue_amount
+			from {{.shopCreatedLogTable}} cr
+			full join {{.shopCompletedLogTable}} com on 
+				cr.shop_id = com.shop_id
+				and cr.day = com.day
+			`,
+			map[string]batch_compute.Table{
+				"shopCreatedLog":   shopCreatedLog,
+				"shopCompletedLog": shopCompletedLog,
+			},
+		)
+
 		dailyTeamHold := batch_compute.NewTableSelect(
 			"daily_team_holds",
 			`
@@ -147,6 +200,24 @@ func NewBatch(
 			`,
 			map[string]batch_compute.Table{
 				"teamOrderHoldLog": teamOrderHoldLog,
+			},
+		)
+
+		dailyShopHold := batch_compute.NewTableSelect(
+			"daily_shop_holds",
+			`
+			select 
+				th.*,
+				sum(th.created_count - th.completed_count) over (
+					partition by th.shop_id
+					order by th.day asc) as hold_count,
+				sum(th.created_revenue_amount - th.completed_revenue_amount) over (
+					partition by th.shop_id
+					order by th.day asc) as hold_amount
+			from {{.shopOrderHoldLogTable}} th
+			`,
+			map[string]batch_compute.Table{
+				"shopOrderHoldLog": shopOrderHoldLog,
 			},
 		)
 
@@ -166,6 +237,25 @@ func NewBatch(
 			`,
 			map[string]batch_compute.Table{
 				"dailyTeamHold": dailyTeamHold,
+			},
+		)
+
+		lastShopHold := batch_compute.NewTableSelect(
+			"last_daily_shop_holds",
+			`
+			select 
+				dsh.*
+			from {{.dailyShopHoldTable}} dsh
+			join (
+				select
+					max(day) as day,
+					shop_id
+				from {{.dailyShopHoldTable}} dsh
+				group by shop_id
+			) l on l.day = dsh.day and l.shop_id = dsh.shop_id
+			`,
+			map[string]batch_compute.Table{
+				"dailyShopHold": dailyShopHold,
 			},
 		)
 
@@ -210,21 +300,67 @@ func NewBatch(
 				"orderHold": createdOrderHold,
 			},
 		)
+
+		shopHold := batch_compute.NewTableSelect(
+			"created_shop_order_holds",
+			`
+			select 
+				shop_id,
+				sum(tx_count) as tx_count,
+				sum(revenue_amount) as revenue_amount
+			from {{.orderHoldTable}}
+			group by shop_id
+			`,
+			map[string]batch_compute.Table{
+				"orderHold": createdOrderHold,
+			},
+		)
+
 		crossCheckTeamHoldLast := batch_compute.NewTableSelect(
 			"team_hold_err",
 			`
-			select
-				l.team_id,
-				(l.hold_count - c.tx_count) as hold_count_err,
-				(l.hold_amount - c.revenue_amount) as hold_amount_err
-				
-			from {{.lastTeamHoldTable}} l
-			left join {{.teamHoldTable}} c
-				on l.team_id = c.team_id
+			with data as (
+				select
+					l.team_id,
+					(l.hold_count - c.tx_count) as hold_count_err,
+					(l.hold_amount - c.revenue_amount) as hold_amount_err
+					
+				from {{.lastTeamHoldTable}} l
+				left join {{.teamHoldTable}} c
+					on l.team_id = c.team_id
+			)
+			select 
+				*,
+				now() as sync_at
+			from data
 			`,
 			map[string]batch_compute.Table{
 				"teamHold":     teamHold,
 				"lastTeamHold": lastTeamHold,
+			},
+		)
+
+		crossCheckShopHoldLast := batch_compute.NewTableSelect(
+			"shop_hold_err",
+			`
+			with data as (
+				select
+					l.shop_id,
+					(l.hold_count - c.tx_count) as hold_count_err,
+					(l.hold_amount - c.revenue_amount) as hold_amount_err
+					
+				from {{.lastShopHoldTable}} l
+				left join {{.shopHoldTable}} c
+					on l.shop_id = c.shop_id
+			)
+			select 
+				*,
+				now() as sync_at
+			from data
+			`,
+			map[string]batch_compute.Table{
+				"shopHold":     shopHold,
+				"lastShopHold": lastShopHold,
 			},
 		)
 
@@ -235,6 +371,7 @@ func NewBatch(
 			).
 			Compute(ctx,
 				crossCheckTeamHoldLast,
+				crossCheckShopHoldLast,
 			)
 		if err != nil {
 			return err
